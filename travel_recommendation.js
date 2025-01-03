21447fd3b19e8f3e2564a8f258e8b5306e5c57bb
Robertookwu <ookwurobert@gmail.com>
2025-01-03 13:58:17 -0500

    second commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,2 +1,42 @@
+let jsonData ;
 fetch("travel_recommendation_api.json").then((response) => 
-{return response.json()}).then(data => {console.log(data)});
+{return response.json()}).then(data => {jsonData = data;});
+
+
+const search_btn = document.getElementById("search_btn");
+search_btn.addEventListener("click", function(){
+    const search = document.getElementById("search");
+    searchByText(search.value);
+});
+
+const clear_btn = document.getElementById("clear_btn");
+clear_btn.addEventListener("click", function(){
+    const search = document.getElementById("search");
+    search.value = "";
+});
+
+function searchByText(value){
+    const converted = value.toLowerCase();
+    let updated ;
+    switch(converted){
+        case "beach" :
+            updated = "beaches";
+            break;
+        case "temple" :
+            updated = "temples";
+            break;
+        case "country" :
+            updated = "countries";
+            break;
+        default : 
+            updated = converted;
+            break;
+    }
+    if(jsonData[updated]){
+        for(key in jsonData[updated]){
+            console.log(jsonData[updated][key]);
+        }
+    }else{
+
+    }
+}
